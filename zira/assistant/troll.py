c16403d4eae5eab8d1bf1095124619639d522d90
Dev_dark <163681904+source-dark@users.noreply.github.com>
2024-03-20 15:15:39 +0300

    Add files via upload

---
+++ zira/assistant/troll.py
@@ -0,0 +1,30 @@
+import json
+import os
+import re
+
+from telethon.events import CallbackQuery
+
+from zira import zedub
+
+
+@zedub.tgbot.on(CallbackQuery(data=re.compile(b"troll_(.*)")))
+async def on_plug_in_callback_query_handler(event):
+    timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
+    if os.path.exists("./zira/troll.txt"):
+        jsondata = json.load(open("./zira/troll.txt"))
+        try:
+            message = jsondata[f"{timestamp}"]
+            userid = message["userid"]
+            ids = userid
+            if event.query.user_id in ids:
+                reply_pop_up_alert = (
+                    "الرساله مش ليك يا صاحبي 🖤"
+                )
+            else:
+                encrypted_tcxt = message["text"]
+                reply_pop_up_alert = encrypted_tcxt
+        except KeyError:
+            reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات دارك"
+    else:
+        reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات دارك"
+    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
